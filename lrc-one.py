e21313ad487a3523b41c35a5f845d8e12982931a
kskshaf <118548920+kskshaf@users.noreply.github.com>
2024-03-24 16:43:26 +0800

    增加音节时间删除功能

--- lrc-one.py
+++ lrc-one.py
@@ -10,18 +10,33 @@ def lrc_merge(lrc_input):
     last_time = None
     last_line = None
 
+
     for lines in lrc_input:
         ma_time = re.compile(r'\[\d{2}:\d{2}\.\d{2}\]')
-        time = ma_time.match(lines).group()
-        current_lrc = lines.replace(time,'').replace('【','').replace('】','')
+        time = ma_time.match(lines)
+
+        ma_lrc_dur = re.compile(r'<\d{2}:\d{2}\.\d{2}>')
+        lrc_dur = ma_lrc_dur.findall(lines)
+
+        if lrc_dur:
+            for dur in lrc_dur:
+                lines = lines.replace(dur,'')
+
         line = lines
+
+        if time:
+            time = time.group()
+            current_lrc = lines.replace(time,'').replace('【','').replace('】','')
+        else:
+            time = '' # 使 time != last_time
+            current_lrc = lines.replace('【','').replace('】','')
 
         if time == last_time:
             merged_lrc.pop() # 删除重复歌词
             merged_line = last_line + ' （' + current_lrc + '）'
             merged_lrc.append(merged_line)
         else:
-            if current_lrc == '':
+            if current_lrc == '' and time != None:
                 merged_lrc.append(line + '.')
             else:
                 merged_lrc.append(line) #merged_lrc.append(''.join([time + current_lrc])
