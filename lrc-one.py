b74710d6b10d6689265218374909b8b35467c038
kskshaf <118548920+kskshaf@users.noreply.github.com>
2024-10-19 01:35:11 +0800

    处理有三行是同一时间，首行为空行的情况

--- lrc-one.py
+++ lrc-one.py
@@ -10,7 +10,6 @@ def lrc_merge(lrc_input):
     last_time = None
     last_line = None
 
-
     for lines in lrc_input:
         ma_time = re.compile(r'\[\d{2}:\d{2}\.\d{2}\]')
         time = ma_time.match(lines)
@@ -32,9 +31,12 @@ def lrc_merge(lrc_input):
             current_lrc = lines.replace('【','').replace('】','')
 
         if time == last_time:
-            merged_lrc.pop() # 删除重复歌词
-            merged_line = last_line + ' （' + current_lrc + '）'
-            merged_lrc.append(merged_line)
+            if last_line.replace(time,'') != '':
+                merged_lrc.pop() # 删除重复歌词
+                merged_line = last_line + '（' + current_lrc + '）'
+                merged_lrc.append(merged_line)
+            else:
+                last_line = last_line + current_lrc # 处理有三行是同一时间，首行为空行的情况
         else:
             if current_lrc == '' and time != None:
                 merged_lrc.append(line + '.')
